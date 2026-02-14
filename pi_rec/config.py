11fbc7548e54a1a30a5ff87edb5f192bf902a281
nikloynes <nl1676@nyu.edu>
2026-02-14 23:58:35 +0000

    making a change to enforce adding device_name

--- pi_rec/config.py
+++ pi_rec/config.py
@@ -22,7 +22,9 @@ class AudioSettings(BaseSettings):
     channels: int = Field(default=2)
     chunk_size: int = Field(default=4096)
     device_index: int | None = Field(default=None)
-    device_name: str | None = Field(default=None)
+    device_name: str | None = Field(
+        default=None, description="the name of the usb device"
+    )
 
 
 class RecordingSettings(BaseSettings):
